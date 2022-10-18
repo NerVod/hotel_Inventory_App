85e1e13ca3665ee91edd78a7015a399cf9208740
NerVod <benjamin.jeannerot@gmail.com>
2022-10-18 11:33:22 +0200

    main pipes & parameter LOCALE_ID

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -28,6 +28,7 @@ export class RoomsComponent implements OnInit {
     photos: 'https://images.unsplash.com/photo-1591088398332-8a7791972843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80',
     checkinTime: new Date('18-Oct-2022'),
     checkoutTime: new Date('20-Oct-2022'),
+    rating: 2.54846541,
   },
   {
     roomNumber: 102,
@@ -37,6 +38,7 @@ export class RoomsComponent implements OnInit {
     photos: 'https://images.unsplash.com/photo-1631049552240-59c37f38802b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
     checkinTime: new Date('18-Oct-2022'),
     checkoutTime: new Date('20-Oct-2022'),
+    rating: 3.4,
   },
   {
     roomNumber: 103,
@@ -46,6 +48,7 @@ export class RoomsComponent implements OnInit {
     photos: 'https://images.unsplash.com/photo-1578683010236-d716f9a3f461?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
     checkinTime: new Date('18-Oct-2022'),
     checkoutTime: new Date('20-Oct-2022'),
+    rating: 4.6,
   },
 
 
