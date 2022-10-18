39bed3bc3e2be9b59b86d1e73c50f9c19c8fe597
NerVod <benjamin.jeannerot@gmail.com>
2022-10-18 10:09:47 +0200

    base _ ngIf / ngFor/ ng Switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   templateUrl: './app.component.html',
+  // template: `<h1>Hello World from inline template</h1>
+  // <p>Angular rules !</p>
+  // `,
   styleUrls: ['./app.component.scss']
+  // styles: [`h1 { color: red }`]
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
+
+
+  role= 'Admin';
+
 }
---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { Room, RoomList } from './rooms';
+
+@Component({
+  selector: 'hinv-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.scss']
+})
+export class RoomsComponent implements OnInit {
+
+  hotelName = 'Hilton Hotel';
+
+  numberOfRooms = 10;
+
+  hideRooms = false;
+
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms : 10,
+    bookedRooms : 5
+  }
+
+  roomList: RoomList[] = [ {
+    roomNumber: 101,
+    roomType: "Deluxe Room",
+    amenities: 'Air conditioner, free Wi-fi, Bathroom, kitchen',
+    price: 300,
+    photos: 'https://images.unsplash.com/photo-1591088398332-8a7791972843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80',
+    checkinTime: new Date('18-Oct-2022'),
+    checkoutTime: new Date('20-Oct-2022'),
+  },
+  {
+    roomNumber: 102,
+    roomType: 'Deluxe Room',
+    amenities: 'Air conditionner, Bathroom, wifi jacuzzy, pool View',
+    price: 1000,
+    photos: 'https://images.unsplash.com/photo-1631049552240-59c37f38802b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+    checkinTime: new Date('18-Oct-2022'),
+    checkoutTime: new Date('20-Oct-2022'),
+  },
+  {
+    roomNumber: 103,
+    roomType: 'Private Suite Room',
+    amenities: 'Air conditionner, Bathroom, jacuzzy, pool View, office space, Hammam',
+    price: 15000,
+    photos: 'https://images.unsplash.com/photo-1578683010236-d716f9a3f461?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+    checkinTime: new Date('18-Oct-2022'),
+    checkoutTime: new Date('20-Oct-2022'),
+  },
+
+
+
+]
+  
+  constructor() { }
+  
+  ngOnInit(): void {
+  }
+  
+  toggle() {
+    this.hideRooms = !this.hideRooms
+  }
+}
